93dfac2785e6510bada8ecf8dc1496a2bfbc3bc6
aselefth <andrei200287@gmail.com>
2023-02-03 17:55:52 +0300

    added posts skeletons for home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,14 +4,23 @@ import styles from './HomePage.module.scss'
 import { FC, useState } from 'react'
 import Pagination from '../../components/Pagination/Pagination'
 import { useAppSelector } from '../../hooks/redux'
+import PostSkeleton from '../../skeletons/PostSkeleton/PostSkeleton'
 
 const HomePage: FC = () => {
     const [pageNum, setPageNum] = useState(1)
     const isLogged = useAppSelector((state) => state.authSlice.isLogged)
-    const { data } = useGetAllPostsQuery(pageNum)
+    const { data, isLoading } = useGetAllPostsQuery(pageNum)
 
     return (
         <div className={styles.homePageWrapper}>
+            {isLoading && (
+                <>
+                    <PostSkeleton />
+                    <PostSkeleton />
+                    <PostSkeleton />
+                    <PostSkeleton />
+                </>
+            )}
             {data &&
                 data.page.map((post) => <Post key={post.postId} post={post} />)}
             <Pagination
